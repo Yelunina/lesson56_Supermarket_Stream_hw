e130cc0297d53afd2fc41a67e90d1168905b3290
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-11-29 17:59:41 +0100

    init

---
+++ src/ait/supermarket/dao/Supermarket.java
@@ -0,0 +1,21 @@
+package ait.supermarket.dao;
+
+import ait.supermarket.model.Product;
+
+public interface Supermarket extends Iterable<Product> {
+    boolean addProduct(Product product);
+
+    Product removeProduct(long barCode);
+
+    Product findByBarCode(long barCode);
+
+    Iterable<Product> findByCategory(String category);
+
+    Iterable<Product> findByBrand(String brand);
+
+    Iterable<Product> findProductsWithExpiredDate();
+
+    int skuQuantity();
+
+    Iterable<Product> findProductWithExpDate();
+}
